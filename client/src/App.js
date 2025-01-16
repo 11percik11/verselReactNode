a19e1ed8af57be5f523d1083d2ab9df091b63a82
daniil <rebi7dan@gmail.com>
2025-01-16 21:42:23 +0400

    feat(backend): add path

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // Функция для получения данных с бекенда
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://versel-react-node-dev.vercel.app');
+      setData(response.data);
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Данные с сервера</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.name}</li>  // адаптируй в зависимости от данных
+        ))}
+      </ul>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+
+app.use(cors());  // Разрешаем запросы с любых источников
+app.use(express.json());
 
 app.use('/', (req, res) => {
-    res.send("Server is running!")
+    res.send("Server is running!334242")
 })
 
 app.listen(5000, console.log("Server run on port 5000."))
