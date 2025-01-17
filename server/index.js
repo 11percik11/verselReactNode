bff352630c4243e5115efa531b8c4340efa7bc67
daniil <rebi7dan@gmail.com>
2025-01-17 12:47:43 +0400

    fiss

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const cors = require('cors');
 app.use(cors());  // Разрешаем запросы с любых источников
 app.use(express.json());
 
-app.use('/', require('./router'))
+app.use('/', async (req, res) => {
+    try {
+        // Пример простого запроса для проверки соединения
+        // const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
+        // res.json({ message: 'База данных подключена, количество записей:', count });
+        res.send("Server is running!334242")
+      } catch (error) {
+        res.status(500).json({ error: 'Ошибка соединения с базой данных' });
+      }
+})
 
 app.listen(5000, console.log("Server run on port 5000."))
