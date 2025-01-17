ea36bdba4c8def6926047a261653d337e060a470
daniil <rebi7dan@gmail.com>
2025-01-17 14:11:33 +0400

    2version

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 app.use(cors());  // Разрешаем запросы с любых источников
 app.use(express.json());
 
-app.use('/', async (req, res) => {
-    try {
-        // Пример простого запроса для проверки соединения
-        const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
-        // res.json({ message: 'База данных подключена, количество записей:', count });
-        res.send(count)
-      } catch (error) {
-        res.status(500).json({ error: 'Ошибка соединения с базой данных' });
-      }
-})
-
 app.listen(5000, console.log("Server run on port 5000."))
+
+app.use('/', require('./router'))
+
+
+// app.use('/', async (req, res) => {
+//     try {
+//         // Пример простого запроса для проверки соединения
+//         const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
+//         res.json({ message: 'База данных подключена, количество записей:', count });
+//       } catch (error) {
+//         res.status(500).json({ error: 'Ошибка соединения с базой данных' });
+//       }
+// });
--- server/router.js
+++ server/router.js
@@ -6,9 +6,8 @@ const prisma = new PrismaClient();
 router.get('/user', async (req, res) => {
     try {
         // Пример простого запроса для проверки соединения
-        // const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
-        // res.json({ message: 'База данных подключена, количество записей:', count });
-        res.send("Server is running!334242")
+        const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
+        res.json({ message: 'База данных подключена, количество записей:', count });
       } catch (error) {
         res.status(500).json({ error: 'Ошибка соединения с базой данных' });
       }
