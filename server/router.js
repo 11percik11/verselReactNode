643270b493ea5858b52e8c2bff32cac34d8faf1b
daniil <rebi7dan@gmail.com>
2025-01-18 20:43:04 +0400

    feat(prisma): add push

--- server/router.js
+++ server/router.js
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 router.get('/user', async (req, res) => {
     try {
         // Пример простого запроса для проверки соединения
-        const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
-        res.json({ message: 'База данных подключена, количество записей:', count });
+        const count = await prisma.task.count();  // Замените 'user' на имя вашей модели
+        res.json({ message: 'База данных подключена, количество записей######:', count });
       } catch (error) {
         res.status(500).json({ error: 'Ошибка соединения с базой данных' });
       }
