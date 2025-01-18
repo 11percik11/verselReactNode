7585a1d435498e20896703ff7f4af19ceae44011
daniil <rebi7dan@gmail.com>
2025-01-18 23:28:47 +0400

    fit(def)

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null);  // Теперь data и user - это объекты, а не массивы
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
 
   // Функция для получения данных с бекенда
   const fetchData = async () => {
     try {
-      const response = await axios.get('https://versel-react-node-mongo-jw9cucvaf-11percik11s-projects.vercel.app/user');
+      const response = await axios.get('https://versel-react-node-mongo-jw9cucvaf-11percik11s-projects.vercel.app/task');
       setData(response.data);  // Сохраняем ответ в состояние
     } catch (error) {
       console.error('Ошибка при получении данных:', error);
@@ -18,7 +18,7 @@ function App() {
 
   const fetchUser = async () => {
     try {
-      const userData = await axios.get('https://versel-react-node-mongo-jw9cucvaf-11percik11s-projects.vercel.app/task');
+      const userData = await axios.get('https://versel-react-node-mongo-jw9cucvaf-11percik11s-projects.vercel.app/user');
       setUser(userData.data);  // Сохраняем ответ в состояние
     } catch (error) {
       console.error('Ошибка при получении данных:', error);
@@ -35,7 +35,7 @@ function App() {
       <h1>Данные с сервера</h1>
       <ul>
         {/* Теперь выводим значения из объектов */}
-        <li>{user ? `${user.message} ${user.count}` : 'Загрузка данных для Task...'}</li>
+        <li>{user}</li>
         <li>{data ? `${data.message} ${data.count}` : 'Загрузка данных для User...'}</li>
       </ul>
     </div>
--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ router.get('/user', async (req, res) => {
     try {
         // Пример простого запроса для проверки соединения
         const count = await prisma.user.count();  // Замените 'user' на имя вашей модели
-        res.json({ message: 'База данных подключена, количество User:', count });
+        res.send(count);
       } catch (error) {
         res.status(500).json({ error: 'Ошибка соединения с базой данных' });
       }
